9697424aaab57b5bb4b8480b63bc65e0318f577d
Rasel141 <rrasel141@gmail.com>
2018-07-15 13:57:42 +0600

    setup project

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+         <h1 className="display-4">Title</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
